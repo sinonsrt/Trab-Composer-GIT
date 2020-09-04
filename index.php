0616aa60ccbf07fc6e4ef4581e704391dec38ab9
Lucas Marchiori <sinonsrt@hotmail.com>
2020-09-04 14:15:11 -0300

    Criado menu

--- index.php
+++ index.php
@@ -5,3 +5,11 @@
     $climate = new League\CLImate\CLImate;
 
     $climate->out('Trabalho Composer e GIT');
+
+    $climate->out('[1] Raiz quadrada');
+    $climate->out('[2] Fatorial');
+    $climate->out('[3] Numero ao Quadrado');
+
+    $input = $climate->input('Escolha umas das opções para soma:');
+    $input->accept(['1','2','3']);
+    $menu = $input->prompt();  
